583f9ee84b22e61f5e02420d4723e452e2551f8a
Benjamin Stefan <145866889+Benjamin-Stefan@users.noreply.github.com>
2024-09-16 21:57:38 +0200

    fix: Adjust release.config.js to enable publishing to npm and GitHub releases

--- release.config.js
+++ release.config.js
@@ -81,14 +81,14 @@ Released on: {{date}}
                 changelogFile: "CHANGELOG.md",
             },
         ],
-        //"@semantic-release/npm",
-        //"@semantic-release/github",
-        // [
-        //     "@semantic-release/git",
-        //     {
-        //         assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
-        //         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-        //     },
-        // ],
+        "@semantic-release/npm",
+        "@semantic-release/github",
+        [
+            "@semantic-release/git",
+            {
+                assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            },
+        ],
     ],
 };
