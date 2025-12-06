d9a5704dcf9fa85c5ebffae9577a3b4537ba8dd5
Benjamin Stefan <145866889+Benjamin-Stefan@users.noreply.github.com>
2025-12-06 10:32:15 +0100

    chore(examples): update React example to React 19 and React Router 7 (#85)

--- examples/webusb-react-example/src/App.js
+++ examples/webusb-react-example/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router";
 import HeartRateExample from "./examples/HeartRateExample";
 
 function App() {
